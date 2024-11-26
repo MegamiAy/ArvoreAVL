7387692f846f20d5e3d95565bcaa0ec72a338f9e
Laiz Detros <megami.ay@gmail.com>
2024-11-26 20:14:50 -0300

    feat: increase details

--- src/main.c
+++ src/main.c
@@ -2,34 +2,59 @@
 #include <stdlib.h>
 #include "funcs.h"
 
-int main(){
-	int op, num;
-	
-	printf("Digite a opercao que deseja: \n");
-	printf("1. Incluir\n");
-	printf("2. Pesquisar\n");
-	printf("3. Excluir\n");
-	printf("4. Sair\n");
-	scanf("%i", &op);
-	
-	switch(op){
-		case 1:
-			add();
-			break;
-		case 2:
-      printf("Digite um numero que deseja encontrar: \n");
-      scanf("%i", &num);
-			search(num);
-			break;
-		case 3:
-      printf("Digite um numero que deseja deletar: \n");
-      scanf("%i", &num);
-			del(num);
-			break;
-		case 4:
-			printf("Tchau!");
-			break;
-		default:
-			break;
-	}
+int main() {
+    int op, num;
+    Node* raiz = NULL;
+
+    do {
+        printf("Digite a operacao que deseja: \n");
+        printf("1. Incluir\n");
+        printf("2. Pesquisar\n");
+        printf("3. Excluir\n");
+        printf("4. Exibir ordens\n");
+        printf("5. Sair\n");
+        scanf("%i", &op);
+
+        switch (op) {
+            case 1:
+                printf("Digite o número a ser incluído: ");
+                scanf("%i", &num);
+                raiz = add(raiz, num);
+                break;
+            case 2:
+                printf("Digite o número que deseja encontrar: ");
+                scanf("%i", &num);
+                Node* encontrado = search(raiz, num);
+                if (encontrado != NULL) {
+                    printf("Número encontrado: %d\n", encontrado->valor);
+                } else {
+                    printf("Número não encontrado.\n");
+                }
+                break;
+            case 3:
+                printf("Digite o número a ser deletado: ");
+                scanf("%i", &num);
+                raiz = del(raiz, num);
+                break;
+            case 4:
+                printf("Pré-ordem: ");
+                pre_ord(raiz);
+                printf("\n");
+                printf("Pós-ordem: ");
+                pos_ord(raiz);
+                printf("\n");
+                printf("Ordem: ");
+                em_ord(raiz);
+                printf("\n");
+                break;
+            case 5:
+                printf("Tchau!\n");
+                break;
+            default:
+                printf("Opção inválida.\n");
+                break;
+        }
+    } while (op != 5);
+
+    return 0;
 }
