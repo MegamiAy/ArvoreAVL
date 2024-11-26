7e605447c87a450035efbf23ffa0747ba8ac310d
Laiz Detros <megami.ay@gmail.com>
2024-11-26 20:09:30 -0300

    struct e funcs

--- src/funcs.h
+++ src/funcs.h
@@ -1,8 +1,23 @@
 #ifndef FUNCOES_H
 #define FUNCOES_H
 
-void add();
-int search(int n);
-void del(int n);
+typedef struct node {
+    int val;
+    struct node* left;
+    struct node* right;
+    int height;
+} Node;
+
+Node* add(Node* raiz, int valor);
+Node* search(No* raiz, int valor);
+Node* del(Node* raiz, int valor);
+void pre_ord(Node* raiz);
+void pos_ord(Node* raiz);
+void em_ord(Node* raiz);
+Node* rot_direita(Node* y);
+Node* rot_esquerda(Node* x);
+int max(int a, int b);
+int altura(Node* node);
+int fator_balanceamento(Node* node);
 
 #endif
